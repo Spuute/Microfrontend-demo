476590e2baacf369b45946352fef3c25be05e569
Spuute <spuute@gmail.com>
2022-03-12 17:25:16 +0100

    feat(app): adds job library

--- source/Microfrontend-demo/Program.cs
+++ source/Microfrontend-demo/Program.cs
@@ -1,10 +1,15 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microfrontend_demo;
+using Microfrontend_demo.services;
+using Job.lib;
+using Employee.lib.Pages;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+builder.RootComponents.AddLibrary<Jobs>();
+builder.RootComponents.AddLibrary<Employees>();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
