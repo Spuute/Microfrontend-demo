8129b9d9b2ea8c2a5cf5c64e057ad0b45805db1e
Spuute <spuute@gmail.com>
2022-03-12 18:20:27 +0100

    chore(app-shell): adds card to program.cs

--- source/Microfrontend-demo/Program.cs
+++ source/Microfrontend-demo/Program.cs
@@ -3,13 +3,18 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microfrontend_demo;
 using Microfrontend_demo.services;
 using Job.lib;
+using Job.lib.Components;
 using Employee.lib.Pages;
+using Employee.lib.Components;
+
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.RootComponents.AddLibrary<Jobs>();
 builder.RootComponents.AddLibrary<Employees>();
+builder.RootComponents.AddCards<EmployeeCard>();
+builder.RootComponents.AddCards<JobCard>();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
